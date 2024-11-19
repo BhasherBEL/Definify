b9bc394202c58a87c854d0d8fba756bdce5c9f52
Brieuc Dubois <git@bhasher.com>
2024-11-19 04:25:17 +0200

    Passkeys & svelte-auth

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,9 +1,10 @@
 import { Auth } from '@bhasher/svelte-auth';
 import { db } from '$lib/server/db';
 import * as types from '$lib/server/db/schema';
+import { env } from '$env/dynamic/private';
 
 export default new Auth(db, types, 'session', {
 	name: 'definify',
-	id: 'localhost',
-	origin: 'http://localhost:5173'
+	id: env.DOMAIN,
+	origin: [env.URL]
 });
